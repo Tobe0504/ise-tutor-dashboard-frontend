38e61bb5b4313db4e0a75b1b67b2c0db97ca430e
kizitech <kizitoohani@gmail.com>
2023-12-22 18:04:51 +0100

    added answer

--- src/Containers/SupportFaqs/SupportFaqs.tsx
+++ src/Containers/SupportFaqs/SupportFaqs.tsx
@@ -7,24 +7,36 @@ const SupportFaqs = () => {
   const faqs = [
     {
       question: "How can I upload course materials to the platform?",
-      answer: "",
+      answer: `On your dashboard sidebar, select the option "Courses." You'll find the option to "Create course" and follow the steps to upload course materials.`,
     },
 
     {
       question: "Can I customise the appearance of my course on the platform?",
-      answer: "",
+      answer: `Yes, you can customise your course appearance. Under the "Courses" option, you can customise the cover of your course with any picture of your choice.`,
     },
     {
       question: "Where can I find information about a specific student?",
-      answer: "",
+      answer: `Visit the "Student" section to access all your student information. Click on the "View details" option for a more detailed information about each student.`,
     },
     {
       question: "How can I schedule a tutoring session with a student?",
-      answer: "",
+      answer: `Head to the "Schedule" section on your dashboard, click on "Schedule Session," and follow the prompts.`,
     },
     {
       question: "What types of files can I upload as study resources for my students?",
-      answer: "",
+      answer: `You can upload a variety of file types, including documents (PDFs, Word), videos, links, and more.`,
+    },
+    {
+      question: "Is there a limit to the number of modules and lessons I can create in a course?",
+      answer: ``,
+    },
+    {
+      question: "What support is available if I need course design or content assistance?",
+      answer: `You can contact the support team (support@iseEdtech.com) with specific descriptions of what you need help with, and a team member would reach out to you `,
+    },
+    {
+      question: "How can I view feedback or reviews from students about my courses?",
+      answer: `Access the "Feedback" section to see the reviews and feedback provided by your students`,
     },
   ];
 
