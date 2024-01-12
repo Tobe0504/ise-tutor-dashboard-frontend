72f423b0ead874b01a4a7b2197f419c76e96aef6
Tobe0504 <ezimorahtobenna@gmail.com>
2024-01-12 06:10:20 +0100

    added the new integration for login:

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,46 +1,58 @@
-import classes from "./Button.module.css";
+import classes from './Button.module.css'
+import { CircularProgress } from '@mui/material'
 
 type ButtonPropTypes = {
-  children: React.ReactNode;
-  type?:
-    | "primary"
-    | "secondary"
-    | "tertiary"
-    | "null"
-    | "invalid"
-    | "delete"
-    | "black"
-    | "white";
-  disabled?: boolean;
-  onClick?: React.MouseEventHandler<HTMLButtonElement>;
-};
+    children: React.ReactNode
+    type?:
+        | 'primary'
+        | 'secondary'
+        | 'tertiary'
+        | 'null'
+        | 'invalid'
+        | 'delete'
+        | 'black'
+        | 'white'
+    disabled?: boolean
+    onClick?: React.MouseEventHandler<HTMLButtonElement>
+    loading?: boolean
+}
 
-const Button = ({ children, type, disabled, onClick }: ButtonPropTypes) => {
-  return (
-    <button
-      className={`${classes.button} ${
-        type === "secondary"
-          ? classes.secondary
-          : type === "tertiary"
-          ? classes.tertiary
-          : type === "null"
-          ? classes.null
-          : type === "invalid"
-          ? classes.invalid
-          : type === "delete"
-          ? classes.delete
-          : type === "black"
-          ? classes.black
-          : type === "white"
-          ? classes.white
-          : classes.primary
-      }`}
-      onClick={onClick}
-      disabled={disabled}
-    >
-      {children}
-    </button>
-  );
-};
+const Button = ({
+    children,
+    type,
+    disabled,
+    onClick,
+    loading,
+}: ButtonPropTypes) => {
+    return (
+        <button
+            className={`${classes.button} ${
+                type === 'secondary'
+                    ? classes.secondary
+                    : type === 'tertiary'
+                    ? classes.tertiary
+                    : type === 'null'
+                    ? classes.null
+                    : type === 'invalid'
+                    ? classes.invalid
+                    : type === 'delete'
+                    ? classes.delete
+                    : type === 'black'
+                    ? classes.black
+                    : type === 'white'
+                    ? classes.white
+                    : classes.primary
+            }`}
+            onClick={onClick}
+            disabled={disabled}
+        >
+            {loading ? (
+                <CircularProgress size="1.5rem" color="inherit" />
+            ) : (
+                children
+            )}
+        </button>
+    )
+}
 
-export default Button;
+export default Button
--- src/Containers/LoginPage/LoginPage.tsx
+++ src/Containers/LoginPage/LoginPage.tsx
@@ -1,57 +1,99 @@
-import classes from "./LoginPage.module.css";
-import { useNavigate, Link } from "react-router-dom";
-import login from "../../Assets/Images/login.svg";
-import iseLogo from "../../Assets/Images/iseLogo.svg"
-import Input from "../../Components/Input/Input";
-import Button from "../../Components/Button/Button";
-import OnboardingLayout from "../../Components/OnboardingLayout/OnboardingLayout";
+import classes from './LoginPage.module.css'
+import { useNavigate, Link } from 'react-router-dom'
+import login from '../../Assets/Images/login.svg'
+import iseLogo from '../../Assets/Images/iseLogo.svg'
+import Input from '../../Components/Input/Input'
+import Button from '../../Components/Button/Button'
+import OnboardingLayout from '../../Components/OnboardingLayout/OnboardingLayout'
+import { ChangeEvent, useContext } from 'react'
+import { AuthUserContext } from '../../Context/AuthUserContext'
+import Error from '../../Components/Error/Error'
+import { capitalize } from '../../HelperFunctions/capitalize'
 
 const LoginPage = () => {
-  const navigate = useNavigate();
+    // Context
+    const { signIn, setUserLoginInfo, userLoginInfo, signInRequest } =
+        useContext(AuthUserContext)
 
-  return (
-    <OnboardingLayout image={login}>
-      <section className={classes.container}>
-        <img src={iseLogo} alt="Ise Logo" className={classes.logo} />
-        <div className={classes.header}>
-          <h4>Welcome to iṣẹ́ Edtech</h4>
-          <p>Login with the details sent to your email</p>
-        </div>
-        <form 
-          onSubmit={(e) => {
-            e.preventDefault();
-          }}
-        >
-          <Input label="Email" isRequired placeholder="name@gmail.com" />
-          <span>
-            <Input
-              label="Password"
-              isRequired
-              type="password"
-              placeholder="************"
-            />
-          </span>
+    // Router
+    const navigate = useNavigate()
 
-          <div className={classes.rememberAndForgot}>
-            <div>
-              <input type="checkbox" id="remember" />
-              <span>Remember Me</span>
-            </div>
-            <Link to="/forgot-password">Forgot Password?</Link>
-          </div>
+    // Utils
+    const changeHandler = (e: any) => {
+        setUserLoginInfo((prevState) => {
+            return { ...prevState, [e.target.name]: e.target.value }
+        })
+    }
 
-          <div className={classes.buttonSection}>
-            <Button
-              onClick={() => {
-                navigate("/tutor-complete-profile");
-              }}>
-                <span>Log in</span>
-              </Button>
-          </div>
-        </form>
-      </section>
-    </OnboardingLayout>
-  );
-};
+    return (
+        <OnboardingLayout image={login}>
+            <section className={classes.container}>
+                <img src={iseLogo} alt="Ise Logo" className={classes.logo} />
+                <div className={classes.feedback}>
+                    {signInRequest.data && (
+                        <Error type="success">
+                            {capitalize(signInRequest?.data as string)}
+                        </Error>
+                    )}
 
-export default LoginPage;
+                    {signInRequest.error && (
+                        <Error type="error">
+                            {capitalize(signInRequest?.error as string)}
+                        </Error>
+                    )}
+                </div>
+                <div className={classes.header}>
+                    <h4>Welcome to iṣẹ́ Edtech</h4>
+                    <p>Login with the details sent to your email</p>
+                </div>
+                <form
+                    onSubmit={(e) => {
+                        e.preventDefault()
+                    }}
+                >
+                    <Input
+                        label="Email"
+                        isRequired
+                        placeholder="name@gmail.com"
+                        name="email"
+                        value={userLoginInfo.email}
+                        onChange={changeHandler}
+                    />
+                    <span>
+                        <Input
+                            label="Password"
+                            isRequired
+                            type="password"
+                            placeholder="************"
+                            name="password"
+                            value={userLoginInfo.password}
+                            onChange={changeHandler}
+                        />
+                    </span>
+
+                    <div className={classes.rememberAndForgot}>
+                        <div>
+                            <input type="checkbox" id="remember" />
+                            <span>Remember Me</span>
+                        </div>
+                        <Link to="/forgot-password">Forgot Password?</Link>
+                    </div>
+
+                    <div className={classes.buttonSection}>
+                        <Button
+                            onClick={() => {
+                                // navigate('/tutor-complete-profile')
+                                signIn()
+                            }}
+                            loading={signInRequest.isLoading}
+                        >
+                            <span>Log in</span>
+                        </Button>
+                    </div>
+                </form>
+            </section>
+        </OnboardingLayout>
+    )
+}
+
+export default LoginPage
