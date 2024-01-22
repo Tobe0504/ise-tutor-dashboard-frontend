4559c78e78aed6a16304b4966dc209194aa78e61
kizitech <kizitoohani@gmail.com>
2024-01-22 17:42:48 +0100

    Added the student quiz page on the tutor management dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -15,39 +15,33 @@ import CollaborationHubVideoGuide from './Containers/CollaborationHubVideoGuide/
 import StudentPage from './Pages/StudentPage'
 import StudentDetailsPage from './Pages/StudentDetailsPage'
 import CoursesPage from './Pages/CoursesPage'
+import Quiz from './Pages/Quiz'
+
 function App() {
    return (
       <Routes>
          <Route path="*" element={<ErrorPage />} />
          <Route path="/" element={<Navigate to="/dashboard"></Navigate>} />
          <Route path="/dashboard" element={<Dashboard />} />
+
          <Route path="/login-email" element={<LoginEmailTemplatePage />} />
          <Route path="/sign-in" element={<Login />} />
          <Route path="/forgot-password" element={<ForgotPassword />} />
+
          <Route path="/profile-info" element={<TutorProfilePage />} />
-         <Route
-            path="/tutor-complete-profile"
-            element={<TutorCompleteProfilePage />}
-         />
-         <Route
-            path="/tutor-dashboard-loader"
-            element={<TutorDashboardLoadingScreenPage />}
-         />
+         <Route path="/tutor-complete-profile" element={<TutorCompleteProfilePage />} />
+         <Route path="/tutor-dashboard-loader" element={<TutorDashboardLoadingScreenPage />} />
+
          <Route path="/collaboration-hub" element={<CollaborationHubPage />} />
-         <Route
-            path="/collaboration-hub-video-guide"
-            element={<CollaborationHubVideoGuide />}
-         />
-         <Route
-            path="/support/contact-support"
-            element={<ContactSupportPage />}
-         />
+         <Route path="/collaboration-hub-video-guide" element={<CollaborationHubVideoGuide />} />
+
+         <Route path="/support/contact-support" element={<ContactSupportPage />} />
          <Route path="/support" element={<TutorSupport />} />
+
          <Route path="/student" element={<StudentPage />} />
-         <Route
-            path="/student/details/:studentId"
-            element={<StudentDetailsPage />}
-         />
+         <Route path="/student/details/:studentId" element={<StudentDetailsPage />} />
+         <Route path="/student/quiz" element={<Quiz />} />
+
          <Route path="/courses" element={<CoursesPage />} />
       </Routes>
    )
---
+++ src/Containers/QuizPageContainer/QuizPageContainer.tsx
@@ -0,0 +1,118 @@
+import DropdownWithSearch from '../../Components/DropdownWithSearch/DropdownWithSearch'
+import HelloUser from '../../Components/HelloUser/HelloUser'
+import classes from './QuizPageContainer.module.css'
+
+const QuizPageContainer = () => {
+    const quiz = [
+        {
+            courseName: "Frontend development",
+            quizName: "Quiz 2.0",
+            lastCompletionDate: "19th Oct. 2023",
+            noOfResults: 33,
+            lessonName: "Quiz knowldge 1.2",
+        },
+        {
+            courseName: "Frontend development",
+            quizName: "Quiz 2.0",
+            lastCompletionDate: "19th Oct. 2023",
+            noOfResults: 33,
+            lessonName: "Quiz knowldge 1.2",
+        },
+        {
+            courseName: "Frontend development",
+            quizName: "Quiz 2.0",
+            lastCompletionDate: "19th Oct. 2023",
+            noOfResults: 33,
+            lessonName: "Quiz knowldge 1.2",
+        },
+        {
+            courseName: "Frontend development",
+            quizName: "Quiz 2.0",
+            lastCompletionDate: "19th Oct. 2023",
+            noOfResults: 33,
+            lessonName: "Quiz knowldge 1.2",
+        },
+    ];
+
+    return (
+        <div className={classes.container}>
+            <HelloUser header="Quiz" paragraph="This shows an overview of all the quiz report in the course(s) you offer. To see a detailed report of individual quiz, go to students." notIncludePay notIncludeBg />
+
+            <div className={classes.quizHeaderDropdowns}>
+                <DropdownWithSearch
+                    isRequired
+                    label="Select course "
+                    title="Frontend development"
+                    options={[]}
+                />
+
+                <DropdownWithSearch
+                    isRequired
+                    label="Completion date"
+                    title="1 week ago"
+                    options={[]}
+                />
+            </div>
+            <div className={classes.quizDataTableContainer}>
+                <p>Showing 1-4 of 4 quizzes</p>
+                <div className={classes.tableHeader}>
+                    <p>S/N</p>
+                    <span>Course name</span>
+                    <span>Quiz name</span>
+                    <span>Last completion date</span>
+                    <span># of results</span>
+                    <span>Lesson name</span>
+                </div>
+                <div className={classes.tableBodyContainer}>
+                    {quiz.map((data, i) => {
+                        return (
+                            <div className={classes.tableBody} key={i}>
+                                <p>{i + 1}</p>
+                                <span>{data.courseName}</span>
+                                <span>{data.quizName}</span>
+                                <span>{data.lastCompletionDate}</span>
+                                <span>{data.noOfResults}</span>
+                                <span>{data.lessonName}</span>
+                            </div>
+                        );
+                    })}
+                </div>
+                <div className={classes.pageButtons}>
+                    <span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                        >
+                            <path
+                                d="M15 19L8 12L15 5"
+                                stroke="#d8d8d8"
+                                strokeWidth="2"
+                                stroke-linecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </span>
+                    <button>1</button>
+                    <span>
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            fill="none"
+                        >
+                            <path
+                                d="M9 5L16 12L9 19"
+                                stroke="#d8d8d8"
+                                strokeWidth="2"
+                                stroke-linecap="round"
+                                strokeLinejoin="round"
+                            />
+                        </svg>
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default QuizPageContainer
