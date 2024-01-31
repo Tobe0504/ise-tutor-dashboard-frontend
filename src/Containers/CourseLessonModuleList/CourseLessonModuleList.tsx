8312431fbe9f69912bc7e6a22dc05b712d0bd01e
kizitech <kizitoohani@gmail.com>
2024-01-31 16:22:16 +0100

    edited the course curriculum  list -media query

--- src/Containers/CourseLessonModuleList/CourseLessonModuleList.tsx
+++ src/Containers/CourseLessonModuleList/CourseLessonModuleList.tsx
@@ -47,25 +47,25 @@ const CourseLessonModuleList = ({
                   }`}
                 key={i}
               >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  onClick={() => {
-                    activeToggler(i, modules, setModules);
-                  }}
-                >
-                  <path
-                    d="M19 9L12 16L5 9"
-                    stroke="#2E2E2E"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
                 <div className={classes.moduleTitleContainer}>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    onClick={() => {
+                      activeToggler(i, modules, setModules);
+                    }}
+                  >
+                    <path
+                      d="M19 9L12 16L5 9"
+                      stroke="#2E2E2E"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
                   <p
                     onClick={() => {
                       activeToggler(i, modules, setModules);
