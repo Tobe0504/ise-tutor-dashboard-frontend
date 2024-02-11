64bf6e0415b71ec7b2c704a4b06c0e05004e6825
kizitech <kizitoohani@gmail.com>
2024-02-11 22:39:34 +0100

    added the created course info to the week module

--- src/Containers/CreatingCourseModulePageContainer/AddModulesDetails/AddModuleDetails.tsx
+++ src/Containers/CreatingCourseModulePageContainer/AddModulesDetails/AddModuleDetails.tsx
@@ -35,7 +35,7 @@ const AddModuleDetails = () => {
                 </div>
                 <Button
                     type='secondary'
-                    onClick={() => setCurrentStepAndSave(currentStep - 1)}
+                    onClick={() => setCurrentStepAndSave(4)}
                 >
                     <svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6 18.5L18 6.5M6 6.5L18 18.5" stroke="#664EFE" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
