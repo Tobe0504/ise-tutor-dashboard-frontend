df8093aead4346039f3e1dd035699e0627359d87
kizitech <kizitoohani@gmail.com>
2024-02-14 14:19:11 +0100

    Pop-up Block
    added the presentation block and the code example block and also adjusted the media queries for the others Pop-up Block

--- src/Containers/CourseCustomize/AddCodeExample/AddCodeExample.tsx
+++ src/Containers/CourseCustomize/AddCodeExample/AddCodeExample.tsx
@@ -1,3 +1,4 @@
+import Button from '../../../Components/Button/Button';
 import classes from './AddCodeExample.module.css'
 
 type AddCodeExampleProps = {
@@ -7,7 +8,19 @@ type AddCodeExampleProps = {
 
 const AddCodeExample = ({ onClick, onClick2 }: AddCodeExampleProps) => {
     return (
-        <div className={classes.container}>AddCodeExample</div>
+        <div className={classes.container}>
+            <div>
+                <h3>Code example block</h3>
+                <p>Code examples display code in your lessons with the appropriate syntax highlighting for the language you choose. This code cannot be executed.</p>
+            </div>
+
+            <h1>Text Editor</h1>
+
+            <div className={classes.buttonContainer}>
+                <Button type='secondary' onClick={onClick}>Cancel</Button>
+                <Button type='primary' onClick={onClick2}>Save</Button>
+            </div>
+        </div>
     )
 }
 
