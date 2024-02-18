9a419a63553b84b72e61021bdf09491323658b3e
kizitech <kizitoohani@gmail.com>
2024-02-18 01:16:40 +0100

    added the states for the submit request stages

--- src/Containers/CourseCurriculumFeedbackInformationBanner/CourseCurriculumFeedbackInformationBanner.tsx
+++ src/Containers/CourseCurriculumFeedbackInformationBanner/CourseCurriculumFeedbackInformationBanner.tsx
@@ -15,6 +15,11 @@ const CourseCurriculumFeedbackInformationBanner = () => {
     const [displaySubmissionDisclaimerModal, setDisplaySubmissionDisclaimerModal] = useState(false)
     const [displaySubmissionSuccessfulModal, setDisplaySubmissionSuccessfulModal] = useState(false)
 
+    const [submitRequestError, setSubmitRequestError] = useState(true)
+    const [submitRequestError2, setSubmitRequestError2] = useState(false)
+    const [submitRequestProcessing, setSubmitRequestProcessing] = useState(false)
+    const [submitRequestApproved, setSubmitRequestApproved] = useState(false)
+
     return (
         <>
             {displaySubmissionDisclaimerModal && (
@@ -30,6 +35,8 @@ const CourseCurriculumFeedbackInformationBanner = () => {
                             onClick2={() => {
                                 setDisplaySubmissionDisclaimerModal(false);
                                 setDisplaySubmissionSuccessfulModal(true);
+                                setSubmitRequestProcessing(true);
+                                setSubmitRequestError(false);
                             }}
                         />
                     }
@@ -49,20 +56,51 @@ const CourseCurriculumFeedbackInformationBanner = () => {
                     }
                 />
             )}
-            <Error notShowIndicator type={'approved'} borderRadius='10px'>
-                <div className={classes.errorContainer}>
-                    <h4>Deadline Approaching! ⏰</h4>
-                    <p>Submission deadline is <strong>12th December, 2024</strong>. Please ensure you <strong>upload all module</strong>s and <strong>read the submission guide</strong> before submitting your course.</p>
-                    <div className={classes.buttonContainer}>
-                        <Button onClick={() => {
-                            setDisplaySubmissionDisclaimerModal(true)
-                        }} type='black'>Submit for review</Button>
-                        <Button onClick={() => {
-                            navigate('/courses/courses-curriculum/submission-guide')
-                        }} type='invalid'>Read submission guide</Button>
+            {submitRequestError && (
+                <Error notShowIndicator type={'error'} borderRadius='10px'>
+                    <div className={classes.errorContainer}>
+                        <h4>Deadline Approaching! ⏰</h4>
+                        <p>Submission deadline is <strong>12th December, 2024</strong>. Please ensure you <strong>upload all module</strong>s and <strong>read the submission guide</strong> before submitting your course.</p>
+                        <div className={classes.buttonContainer}>
+                            <Button onClick={() => {
+                                setDisplaySubmissionDisclaimerModal(true)
+                            }} type='black'>Submit for review</Button>
+                            <Button onClick={() => {
+                                navigate('/courses/courses-curriculum/submission-guide')
+                            }} type='invalid'>Read submission guide</Button>
+                        </div>
                     </div>
-                </div>
-            </Error>
+                </Error>
+            )}
+            {submitRequestProcessing && (
+                <Error notShowIndicator type={'processing'} borderRadius='10px'>
+                    <div className={classes.errorContainer}>
+                        <h4>Revision requested</h4>
+                        <p>Great job with the curriculum 🎉. Some modules need revision. For details, select "Feedback". Please revise and resubmit for approval.</p>
+                        <div className={classes.buttonContainer}>
+                            <Button onClick={() => {
+                                navigate('/courses/courses-curriculum/submission-guide')
+                            }} type='invalid'>Read submission guide</Button>
+                        </div>
+                    </div>
+                </Error>
+            )}
+            {submitRequestApproved && (
+                <Error notShowIndicator type={'approved'} borderRadius='10px'>
+                    <div className={classes.errorContainer}>
+                        <h4>Congratulations!</h4>
+                        <p>Course successfully approved. Well done on creating an outstanding curriculum! 🌟</p>
+                    </div>
+                </Error>
+            )}
+            {submitRequestError2 && (
+                <Error notShowIndicator type={'error'} borderRadius='10px'>
+                    <div className={classes.errorContainer}>
+                        <h4>Course under review</h4>
+                        <p>Editing is temporarily unavailable for courses under review. You can make edits once the review is over.</p>
+                    </div>
+                </Error>
+            )}
         </>
     )
 }
