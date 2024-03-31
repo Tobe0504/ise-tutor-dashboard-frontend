325c8575d931ecd13d8b0cafef76e813f594ac34
kizitech <kizitoohani@gmail.com>
2024-03-31 09:35:58 +0100

    added the date filter for the student quiz page

--- src/Containers/QuizPageContainer/QuizPageContainer.tsx
+++ src/Containers/QuizPageContainer/QuizPageContainer.tsx
@@ -3,14 +3,23 @@ import HelloUser from '../../Components/HelloUser/HelloUser'
 import classes from './QuizPageContainer.module.css'
 import calendarIcon from '../../Assets/Images/calendar.svg'
 import { useState } from 'react'
+import EmptyTabComponent from '../../Components/EmptyTabComponent/EmptyTabComponent'
+import noResultFound from "../../Assets/Images/noResult.svg";
+
+interface QuizData {
+    courseName: string;
+    quizName: string;
+    lastCompletionDate: string;
+    noOfResults: number;
+    lessonName: string;
+}
 
 const QuizPageContainer = () => {
-
     // States
     const [courseSelected, setCourseSelected] = useState('')
+    const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
-    // Utils
-    const quizData = [
+    const quizData: QuizData[] = [
         {
             courseName: "Frontend development",
             quizName: "Quiz 2.0",
@@ -35,24 +44,33 @@ const QuizPageContainer = () => {
         {
             courseName: "Frontend development",
             quizName: "Quiz 2.0",
-            lastCompletionDate: "19th Oct. 2023",
+            lastCompletionDate: "31th Mar. 2024",
             noOfResults: 33,
             lessonName: "Quiz knowledge 1.2",
         },
     ];
 
-    const filteredQuizData = courseSelected ? quizData.filter(data => data.courseName === courseSelected) : quizData;
+    const filteredQuizData = quizData.filter(data => {
+        if (courseSelected && data.courseName !== courseSelected) {
+            return false;
+        }
+        if (selectedDate) {
+            const selectedDateString = `${selectedDate.getDate()}th ${selectedDate.toLocaleString('default', { month: 'short' })}. ${selectedDate.getFullYear()}`;
+            return data.lastCompletionDate === selectedDateString;
+        }
+        return true;
+    });
 
     const coursesSet = new Set(quizData.map(data => data.courseName));
     const courses = ['All Courses', ...Array.from(coursesSet)];
 
-
     const handleCourseSelection = (selectedCourse: string) => {
-        if (selectedCourse === 'All Courses') {
-            setCourseSelected('');
-        } else {
-            setCourseSelected(selectedCourse);
-        }
+        setCourseSelected(selectedCourse === 'All Courses' ? '' : selectedCourse);
+    }
+
+    const handleDateSelection = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const selectedDateString = event.target.value;
+        setSelectedDate(selectedDateString ? new Date(selectedDateString) : null);
     }
 
     return (
@@ -71,72 +89,89 @@ const QuizPageContainer = () => {
                     <label htmlFor="completedDate">Completion date</label>
                     <div className={classes.selectDate}>
                         <input
-                            type="text"
+                            type="date"
                             id="completedDate"
                             placeholder="1 week ago"
-                            onFocus={(e) => (e.target.type = 'date')}
+                            onChange={handleDateSelection}
                         />
                         <img src={calendarIcon} alt="select a date" />
                     </div>
                 </div>
             </div>
             <div className={classes.quizDataTableContainer}>
-                <p>Showing {filteredQuizData.length} of {quizData.length} quizzes</p>
-                <div className={classes.tableHeader}>
-                    <p>S/N</p>
-                    <span>Course name</span>
-                    <span>Quiz name</span>
-                    <span>Last completion date</span>
-                    <span># of results</span>
-                    <span>Lesson name</span>
-                </div>
-                <div className={classes.tableBodyContainer}>
-                    {filteredQuizData.map((data, i) => {
-                        return (
-                            <div className={classes.tableBody} key={i}>
-                                <p>{i + 1}</p>
-                                <span>{data.courseName}</span>
-                                <span>{data.quizName}</span>
-                                <span>{data.lastCompletionDate}</span>
-                                <span>{data.noOfResults}</span>
-                                <span>{data.lessonName}</span>
+                {filteredQuizData.length === 0 ? (
+                    <EmptyTabComponent
+                        image={noResultFound}
+                        header={`No quizzes found for the selected criteria.`}
+                        firstParagraph='Try a new search'
+                        imageHeight={280}
+                        route=''
+                        buttonType='null'
+                    />
+                ) : (
+                    <>
+                        <p>Showing {filteredQuizData.length} of {quizData.length} quizzes</p>
+                        <div className={classes.body}>
+                            <div className={classes.tableHeader}>
+                                <p>S/N</p>
+                                <span>Course name</span>
+                                <span>Quiz name</span>
+                                <span>Last completion date</span>
+                                <span># of results</span>
+                                <span>Lesson name</span>
                             </div>
-                        );
-                    })}
-                </div>
-                <div className={classes.pageButtons}>
-                    <span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                        >
-                            <path
-                                d="M15 19L8 12L15 5"
-                                stroke="#d8d8d8"
-                                strokeWidth="2"
-                                stroke-linecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </span>
-                    <button>1</button>
-                    <span>
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            viewBox="0 0 24 24"
-                            fill="none"
-                        >
-                            <path
-                                d="M9 5L16 12L9 19"
-                                stroke="#d8d8d8"
-                                strokeWidth="2"
-                                stroke-linecap="round"
-                                strokeLinejoin="round"
-                            />
-                        </svg>
-                    </span>
-                </div>
+                            <div className={classes.tableBodyContainer}>
+                                {filteredQuizData.map((data, i) => {
+                                    return (
+                                        <div className={classes.tableBody} key={i}>
+                                            <p>{i + 1}</p>
+                                            <span>{data.courseName}</span>
+                                            <span>{data.quizName}</span>
+                                            <span>{data.lastCompletionDate}</span>
+                                            <span>{data.noOfResults}</span>
+                                            <span>{data.lessonName}</span>
+                                        </div>
+                                    );
+                                })}
+                            </div>
+                        </div>
+                    </>
+                )}
+                {filteredQuizData.length > 0 && (
+                    <div className={classes.pageButtons}>
+                        <span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                            >
+                                <path
+                                    d="M15 19L8 12L15 5"
+                                    stroke="#d8d8d8"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </span>
+                        <button>1</button>
+                        <span>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                            >
+                                <path
+                                    d="M9 5L16 12L9 19"
+                                    stroke="#d8d8d8"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                />
+                            </svg>
+                        </span>
+                    </div>
+                )}
             </div>
         </div>
     )
